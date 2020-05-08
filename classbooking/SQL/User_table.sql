3ddc33fd01f056293951047fe48da151ff5d7290
JokerTsang <287195996@qq.com>
2020-05-08 22:00:32 +0800

    上传建表语句

---
+++ classbooking/SQL/User_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `user` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `identity` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `sex` varchar(4) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
+  `userType` varchar(2) NOT NULL,
+  `invitationCode` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
